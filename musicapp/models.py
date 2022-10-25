03e040398a30f98b1808e4117beb93f8a704533a
Ifeoluwa-bit <68379113+Ifeoluwa-bit@users.noreply.github.com>
2022-10-25 05:33:00 -0700

    models updated

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    price =  models.IntegerField(default=0)
+    
+    
+    def __str__(self):
+        return self.first_name
+    
+class Song(models.Model):
+    artiste = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    date_released = models.DateTimeField(auto_now=True)
+    likes = models.IntegerField(default=0)
+    # artiste_id = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
+    
+    def __str__(self):
+        return self.title
+    
+    
+class Lyric(models.Model):
+   song = models.ForeignKey(Song, on_delete=models.CASCADE)
+   content = models.TextField(max_length=5000)
+#    song_id = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
+   
+   def __str__(self):
+        return self.song_id
